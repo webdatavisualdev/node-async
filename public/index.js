a86ee61f851b961b456419ba84fc71aabe073373
webdatavisualdev <cli698638@gmail.com>
2017-12-14 12:33:12 -0800

    removed images in first page

--- public/index.js
+++ public/index.js
@@ -2,7 +2,6 @@ var app = angular.module("app", []);
 
 app.controller("appCtrl", function($scope, $http) {
     $scope.data = {};
-    $scope.images = [];
 
     $scope.init = function() {
         setInterval(function() {
@@ -10,16 +9,6 @@ app.controller("appCtrl", function($scope, $http) {
                 $scope.data = res.data;
             });
         }, 5000);
-
-        $http.get("/image?mark").then(function (res) {
-            $scope.images.push(res.data);
-            $http.get("/image?matt").then(function (res) {
-                $scope.images.push(res.data);
-                $http.get("/image?jon").then(function (res) {
-                    $scope.images.push(res.data);
-                });
-            });
-        });
     }
 
     $scope.init();
